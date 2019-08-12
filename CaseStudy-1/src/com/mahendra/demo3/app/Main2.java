f7138b2044ecc396639fce382a1dd7e79c0f26fc
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-08-12 17:56:19 +0530

    Interest on Deposit

---
+++ CaseStudy-1/src/com/mahendra/demo3/app/Main2.java
@@ -0,0 +1,15 @@
+package com.mahendra.demo3.app;
+
+import com.mahendra.demo3.Customer;
+import com.mahendra.demo3.Deposit;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer(101,"Robert Downy Jr","rdj@marvel.com");
+		Deposit d = new Deposit(customer, 108080, 10000, 9, 36);
+		
+		d.printInterest();
+	}
+
+}
