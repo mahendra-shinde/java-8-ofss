e6e9b52d8ba20d5eaf93098a74795f272da86241
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-08-12 17:46:06 +0530

    static method in interface

--- CaseStudy-1/src/com/mahendra/demo3/InterestCalculator.java
+++ CaseStudy-1/src/com/mahendra/demo3/InterestCalculator.java
@@ -7,5 +7,9 @@ public interface InterestCalculator {
 	default void printInterest() {
 		System.out.println("Interest charged "+ calculate());
 	}
-	
+	static void show(InterestCalculator calc) {
+		System.out.println("Hello World!");
+		System.out.println("Interest charged "+ calc.calculate());
+
+	}
 }
--- CaseStudy-1/src/com/mahendra/demo3/app/Main1.java
+++ CaseStudy-1/src/com/mahendra/demo3/app/Main1.java
@@ -12,8 +12,8 @@ public class Main1 {
 		Account ac1 = new SavingAccount(1001, "Rahul", 14000, 500);
 		Card c1 = new CreditCard(ac1,10029099,"03/25",0.14,100000,34000);
 		ac1.printInterest();
-		InterestCalculator calc = (InterestCalculator)c1;
-		calc.printInterest();
+		
+		InterestCalculator.show((InterestCalculator)c1);
 
 	}
 
