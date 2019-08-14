e7ecce29f2510ba6b3682c4a7cd475b3c71baba2
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-08-14 14:59:52 +0530

    User input

--- CaseStudy-3/src/com/mahendra/demo3/app/Main.java
+++ CaseStudy-3/src/com/mahendra/demo3/app/Main.java
@@ -1,5 +1,9 @@
 package com.mahendra.demo3.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.mahendra.demo3.dao.AccountDAO;
 import com.mahendra.demo3.dao.CardDAO;
 import com.mahendra.demo3.dao.DataStore;
@@ -26,10 +30,40 @@ public static void main(String[] args) {
 		System.out.println(c);
 	}
 	
-	Account a1 =new SavingAccount(102,"Abcd",10000,1000);
-	dao1.save(a1);
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter account details: ");
+	System.out.println("Account No: ");
+	int accNo=0;
+	String holderName=null;
+	double balance=0;
+	try {
+		String s1 = br.readLine();
+		accNo = Integer.parseInt(s1);
+		System.out.println("Enter account holder name: ");
+		holderName = br.readLine();
+		System.out.println("Enter balance: ");
+		String s2 = br.readLine();
+		balance = Double.parseDouble(s2);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
-	dao2.save(new CreditCard(a1, 12399012, "02/22", 15, 200000, 23000));
+	Account a1 =new SavingAccount(accNo,holderName,balance,1000);
+	dao1.save(a1);
+	int cardNo=0;
+	String exp=null;
+	try {
+		System.out.println("Enter card no ");
+		String s3 = br.readLine();
+		cardNo= Integer.parseInt(s3);
+		System.out.println("expiry date");
+		exp = br.readLine();
+		
+	}catch(IOException ex) {
+		
+	}
+	dao2.save(new CreditCard(a1, cardNo, exp, 15, 200000, 23000));
 	
 	System.out.println("Write everything to file");
 	store.save();
